93fb9212f9d98164b82464f5665bc74c66a5581c
Tay Tem Hoe <127162870+TayHoem@users.noreply.github.com>
2025-12-10 13:25:43 +0800

    aws live

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cwowny8gru3o.us-east-1.rds.amazonaws.com"
+customuser = "tay_user"
+custompass = "123456T!t"
+customdb = "employee"
+custombucket = "taytemhoe-employee"
+customregion = "us-east-1"
+
